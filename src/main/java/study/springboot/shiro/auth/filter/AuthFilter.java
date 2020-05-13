9c1a6902a63818235d1414a7adb2e51176615260
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-05-13 14:29:06 +0800

    提交代码

---
+++ src/main/java/study/springboot/shiro/auth/filter/AuthFilter.java
@@ -0,0 +1,17 @@
+package study.springboot.shiro.auth.filter;
+
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AuthFilter extends AuthenticatingFilter {
+
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response,
+                                      Object mappedValue) {
+        return super.isAccessAllowed(request, response, mappedValue);
+    }
+}
